bf4038195248b4fa07de142a4a8135a737ecacc2
RustyBalboadev <rustybalboadev@gmail.com>
2020-07-26 00:07:03 -0500

    More Arguments | Error Handling | Abstraction
      -i  --refresh  Refresh Stonks x Amount of Seconds
      -c  --clear    Clear All Stonks
    
    Added Error Handling For Stocks That Do Not Exist

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"os/exec"
 	"strings"
+	"time"
 
 	"github.com/akamensky/argparse"
 	"github.com/fatih/color"
@@ -16,73 +18,78 @@ func main() {
 	stonks := []string{}
 
 	userDir, err := os.UserHomeDir()
-	if err != nil {
-		fmt.Println(err)
-	}
+	handleErr(err)
 	makeFiles(userDir)
 
 	parser := argparse.NewParser("goStock", "goStock Commands")
 	addStonk := parser.String("a", "add", &argparse.Options{Help: "Add a Stonk Using Its Symbol", Required: false})
 	removeStonk := parser.String("r", "remove", &argparse.Options{Help: "Remove a Stonk Using Its Symbol", Required: false})
+	repeatStonk := parser.Int("i", "refresh", &argparse.Options{Help: "Refresh Stonks x Amount of Seconds", Required: false})
+	var clearBool *bool = parser.Flag("c", "clear", &argparse.Options{Help: "Clear All Stonks", Required: false})
 	p := parser.Parse(os.Args)
-	if p != nil {
-		fmt.Print(parser.Usage(p))
-	}
+	handleErr(p)
+
 	if *addStonk != "" {
 		stripped := strings.Trim(*addStonk, " ")
 		f, err := os.OpenFile(userDir+"\\.goStocks\\config.txt", os.O_APPEND, 0600)
-		if err != nil {
-			fmt.Println(err)
-		}
+		handleErr(err)
 		f.WriteString(stripped + "\n")
-		defer f.Close()
+		f.Close()
 	}
 
 	if *removeStonk != "" {
 		stripped := strings.Trim(*removeStonk, " ")
-		data, err := ioutil.ReadFile(userDir + "\\.goStocks\\config.txt")
-		if err != nil {
-			fmt.Println(err)
-		}
-		splitted := strings.Split(string(data), "\n")
-		newData := ""
-		for i := 0; i < len(splitted)-1; i++ {
-			if splitted[i] != stripped {
-				newData += splitted[i] + "\n"
-			}
-		}
+		remove(stripped)
+	}
+
+	if *clearBool {
+		color.Green("All Stonks Have Been Cleared")
 		os.Remove(userDir + "\\.goStocks\\config.txt")
 		makeFiles(userDir)
-		f, err := os.OpenFile(userDir+"\\.goStocks\\config.txt", os.O_APPEND, 0600)
-		if err != nil {
-			fmt.Println(err)
-		}
-		f.WriteString(newData)
 	}
 
 	data, err := ioutil.ReadFile(userDir + "\\.goStocks\\config.txt")
-	if err != nil {
-		fmt.Println(err)
-	}
+	handleErr(err)
 
 	split := strings.Split(string(data), "\n")
 	for i := 0; i < len(split); i++ {
 		stonks = append(stonks, split[i])
 	}
 
+	if *repeatStonk != 0 {
+		for {
+			showStonks(stonks)
+			time.Sleep(time.Duration(*repeatStonk) * time.Second)
+			cmd := exec.Command("cmd", "/c", "cls")
+			cmd.Stdout = os.Stdout
+			cmd.Run()
+		}
+	} else {
+		showStonks(stonks)
+	}
+
+}
+
+func handleErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func showStonks(stonks []string) {
 	for i := 0; i < len(stonks)-1; i++ {
 		resp, err := http.Get("https://query1.finance.yahoo.com/v11/finance/quoteSummary/" + stonks[i] + "?modules=summaryDetail,price")
-		if err != nil {
-			fmt.Println(err)
-		}
+		handleErr(err)
 		b, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println(err)
-		}
+		handleErr(err)
 		body := string(b)
 		var t APIResponse
 		err = json.Unmarshal([]byte(string(body)), &t)
-
+		if t.QuoteSummary.Error.Description != "" {
+			remove(stonks[i])
+			fmt.Println(color.RedString("\n\n!!!Stonk Symbol '" + stonks[i] + "' Is Invalid!!!"))
+			os.Exit(0)
+		}
 		symbol := t.QuoteSummary.Result[0].Price.Symbol
 		bid := fmt.Sprintf("%.2f", t.QuoteSummary.Result[0].SummaryDetail.Bid.Raw)
 		regMarketChange := t.QuoteSummary.Result[0].Price.RegularMarketChange.Raw
@@ -111,22 +118,16 @@ func makeFiles(userDir string) {
 		errDir := os.Mkdir(userDir+"\\.goStocks", 0755)
 
 		exe, err := os.Executable()
-		if err != nil {
-			fmt.Println(err)
-		}
+		handleErr(err)
 		data, err := ioutil.ReadFile(exe)
-		if err != nil {
-			fmt.Println(err)
-		}
+		handleErr(err)
 		err = ioutil.WriteFile(userDir+"\\.goStocks\\goStock.exe", data, 0644)
-		if err != nil {
-			fmt.Println(err)
-		}
+		handleErr(err)
+
 		os.Setenv("goStock", userDir+"\\.goStocks\\goStock.exe")
+
 		fmt.Println("Your Portfolio Files Have Been Created! Get Help By Using The Argument -h")
-		if errDir != nil {
-			fmt.Println(errDir)
-		}
+		handleErr(errDir)
 	}
 	_, makeFile := os.Stat(userDir + "\\.goStocks\\config.txt")
 	if os.IsNotExist(makeFile) {
@@ -135,4 +136,23 @@ func makeFiles(userDir string) {
 			fmt.Println(errFile)
 		}
 	}
+}
+
+func remove(stonk string) {
+	userDir, err := os.UserHomeDir()
+	data, err := ioutil.ReadFile(userDir + "\\.goStocks\\config.txt")
+	handleErr(err)
+	splitted := strings.Split(string(data), "\n")
+	newData := ""
+	for i := 0; i < len(splitted)-1; i++ {
+		if splitted[i] != stonk {
+			newData += splitted[i] + "\n"
+		}
+	}
+	os.Remove(userDir + "\\.goStocks\\config.txt")
+	makeFiles(userDir)
+	f, err := os.OpenFile(userDir+"\\.goStocks\\config.txt", os.O_APPEND, 0600)
+	handleErr(err)
+	f.WriteString(newData)
+	f.Close()
 }
